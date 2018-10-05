2377cf26cbfdda7d32be3d0a0cdd4aa0288ee2f7
rupertlion <rupertlion@gmail.com>
2018-10-05 18:09:23 +0200

    Adding credentials for FB app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -41,8 +41,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   config.omniauth :facebook
-    # Rails.application.credentials.facebook[:app_key],
-    # Rails.application.credentials.facebook[:app_secret]
+    Rails.application.credentials.facebook[:app_key]
+    Rails.application.credentials.facebook[:app_secret]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
